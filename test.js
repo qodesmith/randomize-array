a37a34adbc76ef753f059ca94a96a30140a3587c
Aaron Cordova <theqodesmith@gmail.com>
2018-09-11 10:10:29 -0400

    Added 3rd test.

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
 const randomize = require('./index')
-const numbers = []
-
-for (let i = 0; i < 100; i++) numbers.push(i)
-
+const numbers = [...Array(100).keys()] // https://goo.gl/7sNQah
 const randomized = randomize(numbers)
 
-let test1 = true
-let test2 = true
 
 /*
   Test 1
@@ -15,8 +10,11 @@ let test2 = true
     * Check that each number is from 0 - 99.
     * Check that it's length = 100.
 */
+let test1 = true
+
 if (numbers.length !== 100) test1 = false
 if (!numbers.every((num, i) => num === i)) test1 = false
+console.log('Test 1:', test1 ? 'PASSED' : 'FAILED')
 
 
 /*
@@ -26,9 +24,54 @@ if (!numbers.every((num, i) => num === i)) test1 = false
     * The randomized array should not be the same array as the original array.
     * The randomized array should have it's contents randomized.
 */
+let test2 = true
+
 if (randomized.length !== 100) test2 = false
 if (randomized === numbers) test2 = false
 if (!randomized.some((num, i) => num !== numbers[i])) test2 = false
-
-console.log('Test 1:', test1 ? 'PASSED' : 'FAILED')
 console.log('Test 2:', test2 ? 'PASSED' : 'FAILED')
+
+
+/*
+  Test 3
+  ------
+    * It should throw an error if fed anything other than an array.
+*/
+let test3 = true
+
+try {
+  randomize(5)
+  test3 = false
+} catch(e) {}
+
+try {
+  randomize({})
+  test3 = false
+} catch(e) {}
+
+try {
+  randomize('')
+  test3 = false
+} catch(e) {}
+
+try {
+  randomize(function() {})
+  test3 = false
+} catch(e) {}
+
+try {
+  randomize()
+  test3 = false
+} catch(e) {}
+
+try {
+  randomize(undefined)
+  test3 = false
+} catch(e) {}
+
+try {
+  randomize(null)
+  test3 = false
+} catch(e) {}
+
+console.log('Test 3:', test3 ? 'PASSED' : 'FAILED')
